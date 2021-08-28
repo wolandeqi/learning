d8fbb4ade65c69482d4d3d6d194b9379a9c253ff
wolandeqi <88821666+wolandeqi@users.noreply.github.com>
2021-08-28 23:03:35 +0800

    Add files via upload

--- 鹏哥/鹏哥/源.cpp
+++ 鹏哥/鹏哥/源.cpp
@@ -1,56 +1,192 @@
-//#define _crt_secure_no_warnings 1
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
 //#include <stdio.h>
 //
-////std→standard 标准      io→input&output
-//
-//int main()/*main——主函数，程序入口，有且仅有一个
-//            int——整型，main前int表示main函数调用之后返回一个整型值*/
-//{
-//	printf("hh\n");//printf→print function    （印刷功能）打印函数
-//
-//	//库函数--c语言本身提供的函数
-//
-//	return 0;//返回0
-//}
-
-//#include <stdio.h>
-
 //int main()
-//{
-//	char ch = 'A';
-//	printf("%c", ch);//%c—打印字符格式的数据
-//	return 0;
-//}
 //
-/*  char  字符类型  单引号→字符  双引号→字符串 short→短整型  long→长整型 float→单精度浮点数
-* double→双精度浮点数
-*输出字符→%c    字符串→%s  整型→%d  打印浮点型(小数)→%f  以地址形式打印→%p
-* 打印十六进制数字→%x
-*/
-
-//int main()
 //{
-//	int age = 20;
+//	int num1 = 0;
 //
-//	printf("%d", age);//%d—打印整型十进制数据
+//	int num2 = 0;
+//
+//	int sum = 0;
+//
+//	scanf("%d%d",&num1, &num2);
+//
+//	sum = num1 + num2;
+//
+//	printf("sum=%d", sum);
 //
 //	return 0;
 //}
 
+//直接的值（0,1，100等）字面常量。  ***常量（1）
+//num等为变量
+
 //#include <stdio.h>
+//int main()
+//{
+//	const int num = 4;//const→常属性，给变量设置一个固定的值。   ***常量（2）
+//
+//	printf("%d\n", num);
+//
+////	num = 8;
+////                               （无const可运行）    
+////	printf("%d", num);
+//
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//
+//int main()
+//
+//{
+//	const int n = 10;     //n是变量但是有常属性，称为常变量
+//                                  //有瑕疵
+//	int arr[10] = { 0 };
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#define MAX 10    //#define定义的标识符 常量.   ***常量（3）
+//int main()
+//{
+//	int arr[MAX] = { 0 };
+//	printf("%d\n", MAX);
+//	return 0;
+//
+//}
+
+////枚举常量   ***常量（4）
+////枚举关键→enum
+//enum color
+//{
+//	red,
+//	blue,
+//	green 
+//};
 //
 //int main()
 //{
-//	float d = 3.3;       //double d = 3.3;
-//	printf("%f", d);     //printf("%lf",d);
+//	enum color color = red;
 //	return 0;
 //}
-#include <stdio.h>
+
+//#include <stdio.h>
+//#include <string.h>//  →strlen→string length 计算字符串长度
+//
+//int main()
+//{
+//	char arr1[] = "abc";//双引号字符串
+//
+//	//"abc" → ‘a’‘b’‘c’‘\0’→‘\0’字符串结束标志
+//
+//	char zifu[] = { 'a','b','c',0};//单引号单个字符  ,0为结束标志 无0则一直向下(随机数)，直到0.
+//
+////	printf("%s\n", arr1);
+////	printf("%s\n", zifu);
+//
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(zifu));
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main()
+//{
+//	printf("%d\n", strlen("c:\test\32\test.c"));//这里\32为字符 \ddd，ddd表示1-3个八进制数字
+//	//故不能出现8，9    #不知道为什么有8也能运行，可能8不计入\ddd内#
+//	//\xdd→两个十六进制数字.
+//	return 0;
+//}
+
+
+
+//#define _CRT_SECURE_NO_WARINGS 1
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	printf("想买gsx么？(0/1):");
+//	scanf_s("%d", &a);
+//	if (a == 0)
+//		printf("滚去学习\n");
+//	if (a == 1)
+//		printf("那人车两失\n");
+//	return 0;
+//
+//}
+
+
+//         # 循环语句 #
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int line = 0;
+//	printf("开始学习\n");
+//
+//	
+//	while (line < 21600)
+//	{	printf("日复一日的学习：%d\n", line);
+//	line++;
+//	}
+//	if (line>= 21600)
+//		printf("人没了\n");
+//	return 0;
+//}
  
+
+ //    #函数#              【函数复杂时更适用】
+//#include<stdio.h>
+//
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int a = 200;
+//	int b = 30;
+//	int sum = 0;
+//
+//	sum = Add(a, b);
+//	printf("sum:%d\n", sum);
+//
+//	return 0;
+//
+//}
+
+
+//      # 数组 #
+#include<stdio.h>
 int main()
 {
-	short age = 20;
-
-	printf("%d", age);
+	int arr[10] = {1,2,3,4,5,6,7,8,9,10};//int arr[10]定义一个存放十个整数数字的数组,后面为存放内
+//数组下标默认从0始 0 1 2 3 4 5 6 7 8 9
+//	printf("%d\n", arr[4]);//下标方式访问元素→arr[下标]
+	int i = 0;
+	while (i < 10)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
